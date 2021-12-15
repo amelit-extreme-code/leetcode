b0c0f0c2f81689f80fdca76a45b16c7a7a0e7981
Amelit <muradovt111@gmail.com>
2021-12-15 18:11:10 +0300

    added missing number file

---
+++ missingNumber.js
@@ -0,0 +1,12 @@
+﻿const arr = [0, 1, 2, 3, 5, 6, 7]
+
+const missingNumber = (array) => {
+    let sum = new Array()
+    sum = array.reduce((a, b) => a + b)
+    let lengthOfTheArray = array.length
+    const result = (lengthOfTheArray * (lengthOfTheArray + 1)) / 2 - sum
+    console.log(result)
+    return result
+}
+
+missingNumber(arr)
